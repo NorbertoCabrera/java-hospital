3bb859948025c9224451aa7138f81ea2ca98cd2d
NCabrera <norberto.cabrera.a@gmail.com>
2023-12-08 15:53:05 -0300

    collection streaming added

--- src/main/java/com/solvd/javahospitalapp/Administration.java
+++ src/main/java/com/solvd/javahospitalapp/Administration.java
@@ -7,10 +7,8 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Administration <E extends Employee, D extends Department> implements IFilePrintable {
 
@@ -23,9 +21,7 @@ public class Administration <E extends Employee, D extends Department> implement
     private Set<E> employeesPayments = new LinkedHashSet<E>();//LinkedHashSet doesn't allow repeated values but keep each value's order
     private List<D> departments = new ArrayList<D>();
 
-    public Administration() {
-
-    }
+    public Administration() {}
 
     public void setEmployeePayment(E employee) {
 
@@ -71,4 +67,61 @@ public class Administration <E extends Employee, D extends Department> implement
         };
         lamdaPrint.printToFile();
     }
+
+
+    //Filter doctors based on a condition "their experience"
+    public List<Doctor> getExperiencedDoctors(List<Doctor> doctors){
+        return doctors.stream()
+                .filter(doctor -> doctor.getYearsOfExperience() > 5)
+                .collect(Collectors.toList());
+    }
+
+    //map and get doctor's names
+    public List<String> getDoctorsNames(List<Doctor> doctors){
+        return doctors.stream()
+                .map(Doctor::getFirstName)
+                .collect(Collectors.toList());
+    }
+
+    //Sort doctors by years of experience
+    public List<Doctor> getDoctorsByYearsOfExperience(List<Doctor> doctors){
+        return doctors.stream()
+                .sorted((doc1, doc2) -> Integer.compare(doc1.getYearsOfExperience(), doc2.getYearsOfExperience()))
+                .collect(Collectors.toList());
+    }
+
+
+    public boolean isADoctorWithThisSpecialization(List<Doctor> doctors, String specialization){
+               return doctors.stream()
+                .anyMatch(doctor -> doctor.getSpecialization().equals(specialization));
+    }
+
+    public double getAverageExperienceOfDoctors(List<Doctor> doctors){
+        return   doctors.stream()
+                .mapToInt(Doctor::getYearsOfExperience)
+                .average()
+                .orElse(0.0);
+    }
+
+    //Concatenate the first names of all doctors into a single string
+    public String doctorsNameConcatenation(List<Doctor> doctors){
+                return doctors.stream()
+                      .map(Doctor::getFirstName)
+                      .collect(Collectors.joining(", "));
+    }
+
+
+    //Group doctors by their specialization
+   public Map<String,List<Doctor>> getDoctorBySpecialization(List<Doctor> doctors){
+              return  doctors.stream()
+                     .collect(Collectors.groupingBy(Doctor::getSpecialization));
+   }
+
+   //print a result
+   public void showExperiencedDoctors (List<Doctor> doctors){
+        List<Doctor> experiencedDoctors = getDoctorsByYearsOfExperience(doctors);
+        experiencedDoctors.forEach(System.out::println);
+   }
+
+
 }
